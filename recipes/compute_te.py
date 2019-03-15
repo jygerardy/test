30ef5202f166c0249d1018af545ed8786d61b9bd
admin <admin>
2019-03-15 16:27:55 -0400

    Saved recipe: PLUGINS_DEV.compute_te

--- recipes/compute_te.py
+++ recipes/compute_te.py
@@ -5,8 +5,6 @@ import dataiku,os.path,pyspark
 # TODO: Write here your actual code that computes the outputs
 # NB: DSS supports several kinds of APIs for reading and writing data. Please see doc.
 
-te_df = ... # Compute a Pandas dataframe to write into te
-
 
 # Write recipe outputs
 te = dataiku.Dataset("te")
