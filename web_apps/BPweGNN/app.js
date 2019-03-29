c028e5326ff73668b5f598a2d5b3d901f6176851
admin <admin>
2019-03-29 12:00:43 -0400

    Saved web app test (PLUGINS_DEV.BPweGNN)

--- web_apps/BPweGNN/app.js
+++ web_apps/BPweGNN/app.js
@@ -223,8 +223,3 @@ function init() {
 
 
 
-init();$.getJSON(getWebAppBackendUrl('/first_api_call'), function(data) {
-    console.log('Received data from backend', data)
-    const output = $('<pre />').text('Backend reply: ' + JSON.stringify(data));
-    $('body').append(output)
-});
