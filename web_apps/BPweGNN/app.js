cb4f24dc86facc958ce555291ed33c3dd7694753
admin <admin>
2019-03-29 12:00:27 -0400

    Saved web app test (PLUGINS_DEV.BPweGNN)

--- web_apps/BPweGNN/app.js
+++ web_apps/BPweGNN/app.js
@@ -1,69 +1,230 @@
-/*
- * For more information, refer to the "Javascript API" documentation:
- * https://doc.dataiku.com/dss/latest/api/js/index.html
- */
+const graphContainer = document.getElementById('graph-container');
 
-let exportButton = document.getElementById('export-button');
+function score() {
+    let featuresJson = $('#features-field').val();
+    let selectedVariable = $('#selected').val();
+    let counterFactualsRaw = $('#counter-factuals').val();
+    let increment = $('#increment').val();
+    let isCategorical = $('input[name=feature-type]:checked').val() == 'Categorical';
+    
+    resetOutputs();
 
-exportButton.addEventListener('click', function (event) {
-    let analysisName = document.getElementById('analysis-name');
-    let analysisValue = analysisName.value || '(no analysis name typed)';
-    let datasetName = document.getElementById('dataset-name');
-    let datasetValue = datasetName.value || '(no dataset chosen)';
-    let datasetColumn = document.getElementById('dataset-column');
-    let columnValue = datasetColumn.value || '(no column chosen)';
-    let rowsLimitValue = document.getElementById('rows-limit').checked;
-    let exportValue = document.querySelector('input[name="export"]:checked').value || '(no export format typed)';
+    checkInput(selectedVariable, featuresJson); //check if those three inputs are missing or not well formatted
 
-    alert('Now YOU should code something if you really want an export. For now, your parameters are: ' + analysisValue + ' / ' + datasetValue  + ' / ' + columnValue + ' / ' + rowsLimitValue + ' / ' + exportValue);
-    event.preventDefault();
-});
+    let counterFactuals = parseCounterFactuals(counterFactualsRaw, increment, isCategorical);
+    let grid = generateGrid(selectedVariable, counterFactuals, featuresJson);
 
-/* Fetch dataset sample */
-let fetchButton = document.getElementById('fetch-button');
-let datasetName = document.getElementById('dataset-name');
-let messageContainer = document.getElementById('message');
-let selectedDataset = {};
 
-function displayMessage(messageText, messageClassname) {
-    messageContainer.innerHTML = messageText;
-    messageContainer.className = '';
-    if (messageClassname && messageClassname.length > 0) {
-        messageContainer.className = messageClassname;
+    graphContainer.innerHTML = '<div class="graph-placeholder">Scoring in progress. Wait...</div>';
+    webappBackend.get('score', {grid: JSON.stringify(grid)})
+        .catch(err => displayAndThrowError(err, null))
+        .then(response => extractPredictions(response))
+        .then(predictions => displayGraph(predictions, counterFactuals, isCategorical))
+}
+
+
+function checkInput(selectedVariable, json) {
+    if (isBlank(selectedVariable)) {
+        let message = "Missing variable of interest. Enter the variables you would like to see vary";
+        displayAndThrowError(message, 'selected-feature');
+    };
+    try {
+        JSON.parse(json);
+    } catch (e) {
+        let message = "JSON missing or not well formatted";
+        displayAndThrowError(message, 'features');
+        throw e;
     }
 }
 
-function clearMessage() {
-    displayMessage('');
+
+function resetOutputs() {
+    graphContainer.innerHTML = ''; //clear existing graph
+    $('.error-block').remove();
 }
 
-function displayFailure() {
-    displayMessage('The dataset cannot be retrieved. Please check the dataset name or the API Key\'s permissions in the "Settings" tab of the webapp.', 'error-message');
+function displayAndThrowError(errorMsg, field) {
+    let elt;
+    if (field) {
+        elt = $(`.field-${field}`);
+    } else {
+        elt = $('#text-output');
+    }
+    const errorElt = $('<div class="error-block"></div>').text(errorMsg);
+    $('.graph-placeholder').remove();
+    $('.error-block', elt).remove(); // Note that it's not impossible, clicking fast twice on score that 2 errors happen, let's not duplicate (has the side effect of only displaying one error message per call. Sounds ok)
+    elt.append(errorElt);
+    throw new Error(errorMsg);
 }
 
-function displayDataFrame(dataFrame) {
-    let columnsNames = dataFrame.getColumnNames();
-    let line = '------------------------------';
-    let text = selectedDataset.name + '\n'
-        + line + '\n'
-        + dataFrame.getNbRows() + ' Rows\n'
-        + columnsNames.length + ' Columns\n'
-        + '\n' + line + '\n'
-        + 'Columns names: \n';
-    columnsNames.forEach(function(columnName) {
-        text += columnName + ', ';
+
+function extractPredictions(resp) {
+    return resp.data.results.map(pred => pred.prediction);
+}
+
+
+function displayGraph(results, counterFactuals, isCategorical) {
+    resetOutputs();
+    let data = {
+        x: counterFactuals,
+        y: results
+    };
+
+    if (isCategorical) {
+        data.type = 'bar'
+        data.text = results.map(String)
+    } else {
+        data.type = 'scatter'
+    }
+    console.warn('display graph', graphContainer, data)
+    Plotly.newPlot(graphContainer, [data], {
+        scrollZoom: true,
+        showSendToCloud: true
     });
-    displayMessage(text);
 }
 
-fetchButton.addEventListener('click', function(event) {
-    clearMessage();
-    selectedDataset.name = document.getElementById('dataset-to-fetch').value;
-    dataiku.fetch(selectedDataset.name, function(dataFrame) {
-        selectedDataset.dataFrame = dataFrame;
-        displayDataFrame(dataFrame);
-    }, function() {
-        displayFailure();
+
+function generateGrid(selectedVariable, counterFactuals, featuresJson) {
+    if (isBlank(selectedVariable)) {
+        let message = "Missing variable of interest. Enter the variable you would like to vary";
+        displayAndThrowError(message, 'selected-feature');
+    };
+    let grid = {
+        "items": []
+    };
+    for (let cf of counterFactuals) {
+        let features = JSON.parse(featuresJson);
+        features[selectedVariable] = cf;
+        grid.items.push({features: features});
+    }
+    return grid
+}
+
+function parseCounterFactuals(counterFactualsRaw, increment, isCategorical) {
+    if (isCategorical) {
+        return parseCounterFactualsCat(counterFactualsRaw);
+    } else {
+        return parseCounterFactualsNum(counterFactualsRaw, increment);
+    }
+}
+
+
+function parseCounterFactualsCat(counterFactualsRaw) {
+    if (isBlank(counterFactualsRaw)) {
+        let message = "Field is empty!";
+        displayAndThrowError(message, 'error-block-counter-factuals');
+    };
+
+    let cfComma = counterFactualsRaw.split(",");
+    for (let txt of cfComma) {
+        if (isBlank(txt)) {
+            let message = "Missing value in your comma-separated list";
+            displayAndThrowError(message, 'counter-factuals');
+        }
+    };
+
+    return cfComma;
+}
+
+
+function parseCounterFactualsNum(counterFactualsRaw, increment) {
+    if (isBlank(counterFactualsRaw)) {
+        let message = "Field is empty";
+        displayAndThrowError(message, 'counter-factuals');
+    } else {
+        let cfComma = counterFactualsRaw.split(",");
+        let cfDash = counterFactualsRaw.split("-");
+        
+        if (cfComma.length > 1 && cfDash.length > 1) {
+            let message = "Mix of comma(s) and range(s). Only one type allowed";
+            displayAndThrowError(message, 'counter-factuals');
+
+        } else if (cfComma.length > 1) {
+            // this is a comma separated list
+            for (let txt of cfComma) {
+                if (isBlank(txt)) {
+                    //  handle empty values before "Numberizing" them as
+                    //  empty values are converted to 0 when Numberizing them
+                    let message = "Missing number in your comma-separated list";
+                    displayAndThrowError(message, 'counter-factuals');
+                }
+            };
+            cfComma = cfComma.map(x => Number(x));
+            for (let num of cfComma) {
+                if (isNaN(num)) {
+                    let message = "One of the inputs in your comma-separated list is not a number";
+                    displayAndThrowError(message, 'counter-factuals');
+                }
+            };
+            return cfComma;
+
+        } else if (cfDash.length == 2) {
+
+            for (let txt of cfDash) {
+                if (isBlank(txt)) {
+                    //  handle empty values before "Numberizing" them as
+                    //  empty values are converted to 0 when Numberizing them
+                    let message = "Missing upper or lower bound in your range";
+                    displayAndThrowError(message, 'counter-factuals');
+                }
+            };
+            // this is a range. THere should only be one
+            cfDash = cfDash.map(x => Number(x));
+            for (let num of cfDash) {
+                if (isNaN(num)) {
+                    let message = "One of the inputs in your range is not a number"
+                    displayAndThrowError(message, 'counter-factuals');
+                }
+            }
+            return convertRangeToList(cfDash, increment);
+
+        } else {
+            let message = "Something wrong. Possibly not enough numbers or more than one range";
+            displayAndThrowError(message, 'counter-factuals');
+        }
+    };
+}
+
+
+function convertRangeToList(range, increment) {
+    let minimum = Math.min(...range);
+    let maximum = Math.max(...range);
+
+    if (isBlank(increment) || isNaN(increment)) {
+        let message = "When using a range, be sure to include a numerical increment";
+        displayAndThrowError(message, 'increment');
+    };
+
+    let values = [];
+    for (let v = minimum; v <= maximum; v += increment) {
+        values.push(v);
+    }
+    return values
+}
+
+
+function init() {
+    try {
+        dataiku.checkWebAppParameters();
+    } catch (e) {
+        webappMessages.displayFatalError(e.message + ' Go to settings tab.');
+        throw e;
+    }
+    $('#scoring-button').click(score);
+    $('input[name=feature-type]').change(function () {
+        let isCategorical = $('input[name=feature-type]:checked').val() == 'Categorical';
+        if (isCategorical) {
+            $('.field-increment').hide();
+        } else {
+            $('.field-increment').show();
+        }
     });
-    return false;
+}
+
+
+
+init();$.getJSON(getWebAppBackendUrl('/first_api_call'), function(data) {
+    console.log('Received data from backend', data)
+    const output = $('<pre />').text('Backend reply: ' + JSON.stringify(data));
+    $('body').append(output)
 });
