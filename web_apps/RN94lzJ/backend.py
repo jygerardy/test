ef1fe1749bf42439bfb4bb9270433aec2b2d5940
admin <admin>
2019-07-29 17:20:25 -0400

    Saved web app test (PLUGINS_DEV.RN94lzJ)

--- web_apps/RN94lzJ/backend.py
+++ web_apps/RN94lzJ/backend.py
@@ -1,5 +1,6 @@
 import dataiku
 import pandas as pd
+from flask import requests
 
 
 # Example:
@@ -8,8 +9,6 @@ import pandas as pd
 
 @app.route('/first_api_call')
 def first_call():
-    mydataset = dataiku.Dataset("REPLACE_WITH_YOUR_DATASET_NAME")
-    mydataset_df = mydataset.get_dataframe(sampling='head', limit=500)
 
     #Pandas dataFrames are not directly JSON serializable, use to_json()
     data = mydataset_df.to_json()
