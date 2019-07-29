ec1461dff3e4eeace555b777f1f3eea152fe4a96
admin <admin>
2019-07-29 18:17:23 -0400

    Saved web app test2 (PLUGINS_DEV.7rteIFr)

--- web_apps/7rteIFr/app.js
+++ web_apps/7rteIFr/app.js
@@ -1,11 +1,15 @@
 
-var modifiedData = {"myData":[1,2,3,4]};
-var sendData =  function (){
-   
+
+
+
+var sendData =  function (){   
+    
+    var modifiedData = {"myData":[1,2,3,4]};
     $.getJSON(getWebAppBackendUrl('/first_api_call'), modifiedData,  function(data) {
         console.log(data.status)
 });
     };
+
 
 
 sendData()
