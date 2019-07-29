caa2235dc4a3a0064ecf8e22923a5dd8a859234b
admin <admin>
2019-07-29 17:22:52 -0400

    Saved web app test (PLUGINS_DEV.RN94lzJ)

--- web_apps/RN94lzJ/app.js
+++ web_apps/RN94lzJ/app.js
@@ -1,6 +1,6 @@
 
 function() {
-let data = {"myData": [1,2,3,4,5]};
+const data = {"myData": [1,2,3,4,5]};
 $.getJSON(getWebAppBackendUrl('/first_api_call'), function(data) {
     console.log('sent to backend')
 });} 
