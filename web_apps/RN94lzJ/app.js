b37c6a4bdbd9c194ac33f914492b7c23e5e46357
admin <admin>
2019-07-29 17:34:28 -0400

    Saved web app test (PLUGINS_DEV.RN94lzJ)

--- web_apps/RN94lzJ/app.js
+++ web_apps/RN94lzJ/app.js
@@ -5,7 +5,7 @@
 var sendData = function () {
     var data = {"myData": [1,2,3,4,5]};
     $.getJSON(getWebAppBackendUrl('/first_api_call'), function(data) {
-    console.log('sent to backend')
+    console.log(data)
     });
 };
 
