f299136f916d21558dfc0cccba3d3d82e74b0073
admin <admin>
2019-07-29 18:12:19 -0400

    Saved web app test2 (PLUGINS_DEV.7rteIFr)

--- web_apps/7rteIFr/app.js
+++ web_apps/7rteIFr/app.js
@@ -1,7 +1,8 @@
 
 
-var data = {"myData":[1,2,3,4]};
+
 $(document).ready(function () {
+    var data = {"myData":[1,2,3,4]};
     $.getJSON(getWebAppBackendUrl('/first_api_call'), function(data) {
 });
     };
