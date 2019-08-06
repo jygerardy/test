8c2ddc96deab435efa61da8366b5a8962e307cfe
admin <admin>
2019-08-06 15:41:23 -0500

    Saved web app push-pull (PLUGINS_DEV.XkP9lkd)

--- web_apps/XkP9lkd/backend.py
+++ web_apps/XkP9lkd/backend.py
@@ -1,10 +1,25 @@
 import dataiku
 import pandas as pd
+import dataikuapi
+
 
 
 # Example:
 # From JavaScript, you can access the defined endpoints using
 # getWebAppBackendUrl('first_api_call')
+
+auto_hostname = 'http://localhost:30500'
+auto_api_key = 'tGrJvlIQZDRjLnOq5DrftqWUs1UMxCYC'
+
+client_auto = dataikuapi.DSSClient(auto_hostname, auto_api_key)
+client_design = dataiku.api_client()
+
+
+
+def list_bundles(bundles):
+    return 
+
+
 
 @app.route('/first_api_call')
 def first_call():
@@ -14,3 +29,16 @@ def first_call():
     #Pandas dataFrames are not directly JSON serializable, use to_json()
     data = mydataset_df.to_json()
     return json.dumps({"status": "ok", "data": data})
+
+
+@app.route('/list_bundles')
+def list_bundles():
+    project = dataiku.get_custom_variables().get('projectKey')
+    if project not in client_auto.list_projects()
+        return json.dumps({"status": "missing_auto_project"})
+    else:
+        project_design = client_design.get_project(project)
+        project_auto = client_auto.get_project(project)
+        bundles_auto = project_auto.list_importe
+    
+    
