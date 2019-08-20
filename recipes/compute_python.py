3a4d0798ccb9f53ee2a6d3e87485b1405d78b578
admin <admin>
2019-08-20 06:43:56 -0500

    Recipe PLUGINS_DEV.compute_python saved through notebook notebook editor for compute_python

--- recipes/compute_python.py
+++ recipes/compute_python.py
@@ -9,9 +9,19 @@ _in = dataiku.Dataset("distinct_prepared")
 _out = dataiku.Dataset("python")
 
 schema_in = _in.read_schema()
-repr(schema_in)
+schema_out = [{u'type': u'string', u'name': u'Symbol'},
+              {u'type': u'string', u'name': u'Name'},
+              {u'type': u'string', u'name': u'LastSale'},
+              {u'type': u'double', u'name': u'MarketCap'},
+              {u'type': u'string', u'name': u'ADR TSO'},
+              {u'type': u'string', u'name': u'IPOyear'},
+              {u'type': u'string', u'name': u'Sector'},
+              {u'type': u'string', u'name': u'Industry'},
+              {u'type': u'string', u'name': u'Summary Quote'},
+              {u'type': u'string', u'name': u'col_9'}
+             ]
 
-_out.write_schema(schema_in)
+_out.write_schema(schema_out)
 
 
 with _out.get_writer() as writer:
